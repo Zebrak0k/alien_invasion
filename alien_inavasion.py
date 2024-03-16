627f19af9b4df48cca9f736d49c4d750653eb6a8
Zebrakok <ivanbelakov371@gmail.com>
2024-03-16 16:54:06 +0300

    add class settings

--- alien_inavasion.py
+++ alien_inavasion.py
@@ -1,19 +1,27 @@
 import sys
 import pygame
+from settings import Settings
+
 
 class AlienInavasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                     sys.exit()
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
+
 
 if __name__ == '__main__':
     ai = AlienInavasion()
