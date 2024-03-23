d8532a6589d36dfd017ce77fe3b706a3c9cc9430
Zebrakok <ivanbelakov371@gmail.com>
2024-03-23 19:11:29 +0300

    add fleet and move for this

---
+++ alien.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+from settings import Settings
+
+class Alien(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.settings = Settings()
+        self.screen = ai_game.screen
+
+        self.image = pygame.image.load('images/Enemy.png')
+        self.image = pygame.transform.rotate(self.image, 180)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        print(self.rect.y, self.rect.x)
+
+        self.x = float(self.rect.x)
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self, fleet_direction):
+        self.x += (self.settings.alien_speed * fleet_direction)
+        self.rect.x = self.x
--- settings.py
+++ settings.py
@@ -3,10 +3,14 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (0, 0, 0)
-        self.ship_speed = 0.5
+        self.ship_speed = 1
 
         self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (120, 120, 120)
-        self.bullets_allowed = 4
+        self.bullets_allowed = 8
+
+        self.alien_speed = 0.2
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
